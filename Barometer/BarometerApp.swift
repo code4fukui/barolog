473cfa28c379a825890aaebb17247e3cab2f513a
Taisuke Fukuno <fukuno@jig.jp>
2025-09-10 12:40:56 +0900

    Initial Commit

---
+++ Barometer/BarometerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarometerApp.swift
+//  Barometer
+//
+//  Created by 福野　泰介 on 2025/09/10.
+//
+
+import SwiftUI
+
+@main
+struct BarometerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
